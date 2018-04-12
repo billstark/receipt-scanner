f61fb8ca7308c218299adc1158cf24e07c391e9f
Yang Zhuohan <billstark1996@gmail.com>
2018-04-12 14:51:33 +0800

    change directory

--- ReceiptProcessor/test_processor.py
+++ ReceiptProcessor/test_processor.py
@@ -1,2 +1,7 @@
-from ../ReceiptGenerator/draw_receipt import draw_receipt_with_letter_boxes
+import os
 import numpy as np
+from ReceiptGenerator.draw_receipt import draw_receipt_with_letter_boxes
+from CNNModel.CNN.image_classifier import classify
+
+
+print 'test'
