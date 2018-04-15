4d7e9e007fba93149619a1d5691aabd73ffbdffc
Yang Zhuohan <billstark1996@gmail.com>
2018-04-15 16:33:24 +0800

    update generator

--- ReceiptProcessor/data_generator.py
+++ ReceiptProcessor/data_generator.py
@@ -1,12 +1,27 @@
+import os
 import cv2
 
 from ReceiptGenerator.draw_receipt import create_crnn_sample
 
-NUM_OF_TRAINING_IMAGES = 300
-NUM_OF_TEST_IMAGES = 100
+NUM_OF_TRAINING_IMAGES = 30000
+NUM_OF_TEST_IMAGES = 10000
 
-with open('./training_images/Train/sample.txt', 'w') as input_file:
-    for i in range(0, NUM_OF_TRAINING_IMAGES):
-        img, label = create_crnn_sample()
-        cv2.imwrite('./training_images/Train/{}.jpg'.format(i))
-        input_file.wirte('{}.jpg {}'.format(i, label))
+TEXT_TYPES = ['word', 'word_column', 'word_bracket', 'int', 'float', 'price_left', 'price_right', 'percentage']
+
+with open('./ReceiptProcessor/training_images/Train/sample.txt', 'w') as input_file:
+    for type in TEXT_TYPES:
+        if not os.path.exists('./ReceiptProcessor/training_images/Train/{}'.format(type)):
+            os.mkdir('./ReceiptProcessor/training_images/Train/{}'.format(type))
+        for i in range(0, NUM_OF_TRAINING_IMAGES):
+            img, label = create_crnn_sample()
+            cv2.imwrite('./ReceiptProcessor/training_images/Train/{}/{}.jpg'.format(type, i), img)
+            input_file.write('{}/{}.jpg {}\n'.format(type, i, label))
+
+with open('./ReceiptProcessor/training_images/Test/sample.txt', 'w') as input_file:
+    for type in TEXT_TYPES:
+        if not os.path.exists('./ReceiptProcessor/training_images/Test/{}'.format(type)):
+            os.mkdir('./ReceiptProcessor/training_images/Test/{}'.format(type))
+        for i in range(0, NUM_OF_TEST_IMAGES):
+            img, label = create_crnn_sample()
+            cv2.imwrite('./ReceiptProcessor/training_images/Test/{}/{}.jpg'.format(type, i), img)
+            input_file.write('{}/{}.jpg {}\n'.format(type, i, label))
