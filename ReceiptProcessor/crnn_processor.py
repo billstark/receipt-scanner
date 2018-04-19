b9c14ec15b4bdab0b89b70864004348a46099724
Wu Zefeng <flamesdesperado@gmail.com>
2018-04-19 20:24:51 +0800

    Added reading image and line segmentation; Added outputting text

--- ReceiptProcessor/crnn_processor.py
+++ ReceiptProcessor/crnn_processor.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 import argparse
 import os
+from ReceiptGenerator.line_seg import cut_lines
+from ReceiptProcessor.output_text import output_text
 from CRNNModel.image_classifier import Classifier
 
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -10,13 +12,17 @@ def process_img(img_path):
     path = os.path.join(SCRIPT_PATH, img_path)
     assert os.path.exists(path), 'No such image exists'
 
-    # read images here
+    img = cv2.imread(path)
+
+    _, words, box_list = cut_lines(img)
 
     classifier = Classifier()
 
-    # recognize imgs here
+    text_list = classifier.recognize_imgs(words)
 
-    # write imgs here
+    out_text = output_text(text_list, box_list)
+
+    return out_text
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
