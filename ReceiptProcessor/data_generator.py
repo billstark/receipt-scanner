0780971f5ba7403fa2270a586f6578407e4d9ffc
Yang Zhuohan <billstark1996@gmail.com>
2018-04-19 18:04:09 +0800

    modify data generator

--- ReceiptProcessor/data_generator.py
+++ ReceiptProcessor/data_generator.py
@@ -3,10 +3,11 @@ import cv2
 
 from ReceiptGenerator.draw_receipt import create_crnn_sample
 
-NUM_OF_TRAINING_IMAGES = 30000
-NUM_OF_TEST_IMAGES = 10000
+NUM_OF_TRAINING_IMAGES = 3000
+NUM_OF_TEST_IMAGES = 1000
 
 TEXT_TYPES = ['word', 'word_column', 'word_bracket', 'int', 'float', 'price_left', 'price_right', 'percentage']
+# TEXT_TYPES = ['word']
 
 with open('./ReceiptProcessor/training_images/Train/sample.txt', 'w') as input_file:
     for type in TEXT_TYPES:
