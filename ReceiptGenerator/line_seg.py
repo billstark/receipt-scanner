8069dcdb90438402a7ea4dec8e9745c4fdff91f0
Yang Zhuohan <billstark1996@gmail.com>
2018-04-19 22:53:17 +0800

    enlarge box size

--- ReceiptGenerator/line_seg.py
+++ ReceiptGenerator/line_seg.py
@@ -61,12 +61,12 @@ def cut_lines(image, for_crnn=True):
         bounding_boxes.append(BoundingBox((x, y, w, h)))
 
     height, width, _ = image.shape
-    size_dilate_val = 5
     bounding_boxes = seperate_n_lines(bounding_boxes)
     lines = []
     boxes = []
     for bounding_box in bounding_boxes:
         x, y, w, h = bounding_box.x, bounding_box.y, bounding_box.w, bounding_box.h
+        size_dilate_val = int(h / 10)
         ny = max(y - size_dilate_val, 0)
         ny2 = min(y + h + size_dilate_val, height)
         nx = max(x - size_dilate_val, 0)
